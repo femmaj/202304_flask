a6d294776fb381f3078eb5bf0163b802c488afa5
Fernanda Matthaei <fer.mattj@gmail.com>
2023-06-07 13:33:36 -0400

    ejercicio contador terminado

--- contador/server.py
+++ contador/server.py
@@ -3,19 +3,19 @@ from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
 app.secret_key = "supersecreta"
 
-@app.route("/contador")
+@app.route("/")
 def index():
     if "contador" not in session:
         session["contador"] = 1
     else:
         session["contador"] += 1
-    return render_template("contador.html")
+    return render_template("index.html")
 
 
 @app.route("/destroy_session")
 def reset():
     session.pop("contador")
-    return redirect("index")
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
